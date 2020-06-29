86db88e7209c4ec11ba83a6b702209435b6f99cd
Joseph <joseph@stechstudio.com>
2020-06-29 08:30:43 -0400

    wip

---
+++ database/migrations/2020_06_28_213740_create_themes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+
+            $table->json('layouts')->default('{}');
+            $table->json('sizes')->default('{"medium" : "Medium"}');
+            $table->text("css");
+
+            $table->json('default_layout');
+            $table->json('default_size');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('themes');
+    }
+}
