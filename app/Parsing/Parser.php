062a987e3a5ca5ad87d6a02161520c0ddf1e199e
Joseph Szobody <joseph@stechstudio.com>
2023-01-25 21:50:33 -0500

    add superscript support

--- app/Parsing/Parser.php
+++ app/Parsing/Parser.php
@@ -12,6 +12,11 @@ class Parser extends \ParsedownExtra
     public function __construct(Collection $transformations)
     {
         $this->transformations = $transformations;
+
+        $this->InlineTypes['^'] = ['Superscript'];
+        $this->inlineMarkerList .= "^";
+
+        parent::__construct();
     }
 
     public static function render($input)
@@ -28,5 +33,20 @@ class Parser extends \ParsedownExtra
         }
 
         return parent::lines($lines);
+    }
+
+    protected function inlineSuperscript($Excerpt)
+    {
+        if (preg_match('/^\^(.+?)\^/', $Excerpt['text'], $matches))
+        {
+            return array(
+                'extent' => strlen($matches[0]),
+                'element' => array(
+                    'name' => 'sup',
+                    'text' => $matches[1],
+                    'handler' => 'line',
+                ),
+            );
+        }
     }
 }
